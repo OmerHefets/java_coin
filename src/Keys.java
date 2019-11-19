3041107c9523cd73d681eb1ace6697f1f3fedd2f
Omer Hefets <hefets.omer@gmail.com>
2019-11-19 23:06:17 +0200

    completed privKey

--- src/Keys.java
+++ src/Keys.java
@@ -2,39 +2,21 @@ import java.math.BigInteger;
 import java.util.Random;
 
 class Keys {
+
     public final static int PRIV_KEY_BITS_LENGTH = 256;
-    public final static BigInteger ELIPTIC_CURVE_ORDER = Keys.elipticCurveOrder();
-
-    // Constructor
-    public Keys() {
-
-    }
-
-    public static BigInteger elipticCurveOrder()
-    {
-        BigInteger twoPower256 = new BigInteger("2").pow(256);
-        BigInteger twoPower32 = new BigInteger("2").pow(32);
-        BigInteger twoPower9 = new BigInteger("2").pow(9);
-        BigInteger twoPower8 = new BigInteger("2").pow(8);
-        BigInteger twoPower7 = new BigInteger("2").pow(7);
-        BigInteger twoPower6 = new BigInteger("2").pow(6);
-        BigInteger twoPower4 = new BigInteger("2").pow(4);
-        BigInteger twoPower0 = new BigInteger("2").pow(0);
-        return twoPower256.subtract(twoPower32).subtract(twoPower9).subtract(twoPower8).subtract(twoPower7).
-                subtract(twoPower6).subtract(twoPower4).subtract(twoPower0);
-    }
+    public final static BigInteger ELLIPTIC_CURVE_ORDER = new BigInteger
+            ("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141", 16);
+    public final static BigInteger ELLIPTIC_CURVE_ORDER_MINUS_ONE = ELLIPTIC_CURVE_ORDER.
+            subtract(new BigInteger("1"));
 
 
     public static BigInteger privKey()
     {
         Random randPrivateKey = new Random();
-        BigInteger privateKey = new BigInteger(PRIV_KEY_BITS_LENGTH, randPrivateKey);
-        System.out.println(privateKey);
+        BigInteger privateKey;
+        do {
+            privateKey = new BigInteger(PRIV_KEY_BITS_LENGTH, randPrivateKey);
+        } while (privateKey.compareTo(ELLIPTIC_CURVE_ORDER_MINUS_ONE) > 0);
         return privateKey;
-    }
-
-    public static void main(String[] args)
-    {
-        System.out.print(ELIPTIC_CURVE_ORDER);
     }
 }
